0c92a21a2e542a43f78c2ed4c56052a0cb3b3ec6
Karol <karol.ciesielski87@gmail.com>
2019-03-16 23:07:20 +0100

    Created class: Project and User, created connection to remote MySQL base

---
+++ src/main/java/pl/management/project/project_management/Project.java
@@ -0,0 +1,23 @@
+package pl.management.project.project_management;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+public class Project{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String description;
+    private String administrator;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+}
